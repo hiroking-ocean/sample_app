9206cef853a985bf2e1b4785374789a4aa022df3
Hiroki Tanaka <hariiniya@gmail.com>
2020-02-15 10:56:44 +0900

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user
-      redirect_to user
+      redirect_to user_url(user)
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    
+    log_out
+    redirect_to root_url
   end
 end
